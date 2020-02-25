7a7ab37ed72882acf2c21c00a6cad6f3914c03af
NooraKalpio <noora.kalpio@gmail.com>
2020-02-25 10:55:04 +0200

    .

---
+++ src/app/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossOrigin="anonymous"
+      />
+    </div>
+  );
+}
+
+export default App;
